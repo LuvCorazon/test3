8b3ddd77afe5011955167a37746985e608750b8c
Rustam Yuburov <rt138328@gmail.com>
2025-01-31 17:25:36 +0600

    homework7

--- db/main_db.py
+++ db/main_db.py
@@ -29,26 +29,21 @@ async def sql_insert_detail(product_id, category, info_product):
     ))
     db.commit()
 
+# CRUD - 1
+# ==================================================================
 def get_db_connection():
     conn = sqlite3.connect('db/store.sqlite3')
     conn.row_factory = sqlite3.Row
     return conn
-
 def fetch_all_products():
     conn = get_db_connection()
     products = conn.execute("""
-    SELECT 
-        s.*, 
-        sd.*,
-        c.collection_name,
-        c.collection_description
-    FROM store s
+    SELECT * from store s
     INNER JOIN store_detail sd ON s.product_id = sd.product_id
-    INNER JOIN collection c ON s.collection_id = c.collection_id
+    INNER JOIN collection c ON s.collection_id = c.id
     """).fetchall()
     conn.close()
     return products
-
 def delete_product(product_id):
     conn = get_db_connection()
     conn.execute('DELETE FROM store WHERE product_id = ?', (product_id,))
@@ -56,12 +51,12 @@ def delete_product(product_id):
     conn.commit()
     conn.close()
 
+# CRUD - update
+# ==================================================================
 def update_product_field(product_id, field_name, new_value):
     conn = get_db_connection()
-
-    store_table = ['name_product', 'size', 'price', 'photo', 'collection_id']  # Добавлен collection_id
+    store_table = ['name_product', 'size', 'price', 'photo', 'collection_id']  # Добавлено поле collection_id
     store_details_table = ['category', 'info_product']
-
     try:
         if field_name in store_table:
             query = f"UPDATE store SET {field_name} = ? WHERE product_id = ?"
@@ -69,10 +64,8 @@ def update_product_field(product_id, field_name, new_value):
             query = f"UPDATE store_detail SET {field_name} = ? WHERE product_id = ?"
         else:
             raise ValueError(f'Нет такого поля как {field_name}')
-
         conn.execute(query, (new_value, product_id))
         conn.commit()
-
     except sqlite3.OperationalError as e:
         print(f'Ошибка - {e}')
     finally:
